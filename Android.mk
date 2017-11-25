f0e466b47a9e1e8c7f738135f4091ad983217d8f
maxwen <max.weninger@gmail.com>
2017-11-25 13:27:51 +0100

    Unify calling account display and selection
    * Always also show carrier
    
    Change-Id: I98ea023dac8d59c1cd602775a82b6e6047acb9d1

--- Android.mk
+++ Android.mk
@@ -353,9 +353,7 @@ LOCAL_PROGUARD_ENABLED := disabled
 LOCAL_DX_FLAGS := --multi-dex
 endif
 
-# End Bug: 37077388
-
-LOCAL_SDK_VERSION := system_current
+#LOCAL_SDK_VERSION := system_current
 LOCAL_MODULE_TAGS := optional
 LOCAL_PACKAGE_NAME := Dialer
 LOCAL_CERTIFICATE := shared
